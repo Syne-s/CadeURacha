8cc82c9d4ababedeed6d4df82b34522f68e9a938
Carla Suenne <carla.suenne1@gmail.com>
2024-12-09 16:58:31 -0300

    tela de perfil

--- synes/settings.py
+++ synes/settings.py
@@ -67,11 +67,11 @@ WSGI_APPLICATION = 'synes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'eLBxoXfIyqiYwRxBRSxjNLZtOTsgowRu',
+        'HOST': 'autorack.proxy.rlwy.net',
+        'PORT': '37107',
     }
 }
 
