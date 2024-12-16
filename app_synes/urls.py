566d79ab3700e3be22ae31599c0a04c54456a3df
Jerbesson Silva <jerbessonc@gmail.com>
2024-12-16 17:50:23 -0300

    adição validação campos registro, e função logout

--- app_synes/urls.py
+++ app_synes/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('', views.index, name='index'),
     path('map/', views.map, name='map'),      
 ]
--- app_synes/views.py
+++ app_synes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.http import JsonResponse
 from .models import CustomUser  # Importe o modelo CustomUser
@@ -56,6 +56,10 @@ def login_view(request):
             return JsonResponse({'success': False, 'message': "E-mail ou senha inválidos."})
 
     return render(request, "app_synes/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
 
 def index(request):
     """
