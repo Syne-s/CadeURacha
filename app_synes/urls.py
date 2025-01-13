44d793183e3ae205e7f91e5edfbe3b9527965500
Jerbesson Silva <jerbessonc@gmail.com>
2025-01-13 12:02:19 -0300

    remoção de tudo que envolva reservas de quadra

--- app_synes/urls.py
+++ app_synes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-from .views import cadastrar_jogo, criar_reserva, listar_jogos, listar_reservas
+from .views import cadastrar_jogo, listar_jogos
 
 urlpatterns = [
     path('register/', views.register, name='register'),
@@ -17,16 +17,10 @@ urlpatterns = [
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('confirm_delete_account/', views.confirm_delete_account, name='confirm_delete_account'),
     path('delete_account/', views.delete_account, name='delete_account'),
-    path('cadastrar_jogo/', cadastrar_jogo, name='cadastrar_jogo'),
-    path('reservar/', criar_reserva, name='criar_reserva'),
-    path('reservas/', listar_reservas, name='listar_reservas'),
-    path('reservas/editar/<int:id>/', views.editar_reserva, name='editar_reserva'),  # Adicionada
-    path('reservas/excluir/<int:id>/', views.excluir_reserva, name='excluir_reserva'),  # Adicionada
+    path('cadastrar_jogo/', cadastrar_jogo, name='cadastrar_jogo'),  # Adicionada
     path('jogos/', listar_jogos, name='listar_jogos'),
     path('jogos/editar/<int:id>/', views.editar_jogo, name='editar_jogo'),  # Adicionada
     path('jogos/excluir/<int:id>/', views.excluir_jogo, name='excluir_jogo'),  # Adicionada
-    path('listar_todos_jogos/', views.listar_todos_jogos, name='listar_todos_jogos'),  # Adicionada
-    path('listar_todas_reservas/', views.listar_todas_reservas, name='listar_todas_reservas'),  # Adicionada
+    path('listar_todos_jogos/', views.listar_todos_jogos, name='listar_todos_jogos'),
     path('search/', views.search, name='search'),
-    path('reservas/editar/<int:id>/', views.editar_reserva, name='editar_reserva'),
 ]
