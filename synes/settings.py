a6005f745f50fb4e36eeecc7e9d3e33b2e6ea3cf
Jerbesson Silva <jerbessonc@gmail.com>
2025-02-04 00:50:10 -0300

    remove redundant entry from ALLOWED_HOSTS

--- synes/settings.py
+++ synes/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-m7nz^1020$)xzc1y6r9r5z890@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['cade-u-racha.up.railway.app', 'localhost', '127.0.0.1', 'cadeuracha.up.railway.app']
+ALLOWED_HOSTS = ['cadeuracha.up.railway.app', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://cadeuracha.up.railway.app/']
 
