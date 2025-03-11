7f0c05a3c97df0f902ccabe876cbe7dfaca2650f
Jerbesson Silva <jerbessonc@gmail.com>
2025-03-11 10:10:57 -0300

    add display fields for images in admin; enhance CustomUser and Jogo admin views

--- app_synes/admin.py
+++ app_synes/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
 from .models import Jogo, CustomUser, Arena
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
 
 @admin.register(Jogo)
 class JogoAdmin(admin.ModelAdmin):
-    list_display = ['titulo', 'data', 'horario', 'arena']
+    list_display = ['titulo', 'data', 'horario', 'arena', 'display_imagem']
     search_fields = ['titulo', 'descricao']
     list_filter = ['data', 'arena']
 
@@ -19,9 +20,16 @@ class JogoAdmin(admin.ModelAdmin):
         verbose_name = 'Racha'
         verbose_name_plural = 'Rachas'
 
+    def display_imagem(self, obj):
+        if obj.imagem:
+            return format_html('<a href="{}" target="_blank"><img src="{}" width="50" height="50" style="object-fit: cover;" /><br>Ver imagem</a>', obj.imagem.url, obj.imagem.url)
+        return "Sem imagem"
+    
+    display_imagem.short_description = "Imagem do Racha"
+
 @admin.register(Arena)
 class ArenaAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'endereco', 'cidade', 'estado', 'pais', 'status']
+    list_display = ['nome', 'endereco', 'cidade', 'estado', 'pais', 'status', 'display_foto']
     search_fields = ['nome', 'endereco', 'cidade', 'estado', 'pais']
     list_filter = ['status', 'cidade', 'estado', 'pais']
     
@@ -33,10 +41,17 @@ class ArenaAdmin(admin.ModelAdmin):
         verbose_name = 'Quadra'
         verbose_name_plural = 'Quadras'
 
+    def display_foto(self, obj):
+        if obj.foto_quadra:
+            return format_html('<a href="{}" target="_blank"><img src="{}" width="50" height="50" style="object-fit: cover;" /><br>Ver imagem</a>', obj.foto_quadra.url, obj.foto_quadra.url)
+        return "Sem imagem"
+    
+    display_foto.short_description = "Foto da Quadra"
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
     # Adicionando pode_cadastrar_quadra no list_display
-    list_display = ['username', 'email', 'is_staff', 'is_active', 'pode_cadastrar_quadra', 'is_superuser', 'foto_perfil']
+    list_display = ['username', 'email', 'is_staff', 'is_active', 'pode_cadastrar_quadra', 'is_superuser', 'foto_perfil', 'display_foto_perfil']
     list_filter = ['is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions']
     actions = ['dar_permissao_quadra', 'remover_permissao_quadra']
     
@@ -73,10 +88,10 @@ class CustomUserAdmin(UserAdmin):
     remover_permissao_quadra.short_description = "Remover permissão para cadastrar quadras"
 
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('foto_perfil',)}),
+        (None, {'fields': ('foto_perfil', 'foto_url', 'levar_bola')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('foto_perfil',)}),
+        (None, {'fields': ('foto_perfil', 'foto_url')}),
     )
 
     def has_view_permission(self, request, obj=None):
@@ -110,7 +125,7 @@ class CustomUserAdmin(UserAdmin):
 
     def get_list_display(self, request):
         if request.user.is_superuser:
-            return ['username', 'email', 'is_staff', 'is_active', 'pode_cadastrar_quadra', 'is_superuser', 'foto_perfil']
+            return ['username', 'email', 'is_staff', 'is_active', 'pode_cadastrar_quadra', 'is_superuser', 'foto_perfil', 'display_foto_perfil']
         else:
             # Remove is_superuser, email e foto_perfil da listagem para não superusers
             return ['username', 'is_staff', 'is_active', 'pode_cadastrar_quadra']
@@ -121,6 +136,15 @@ class CustomUserAdmin(UserAdmin):
             return None
         # Se for superuser, mantém o comportamento padrão
         return super().get_list_display_links(request, list_display)
+
+    def display_foto_perfil(self, obj):
+        if obj.foto_url:
+            return format_html('<a href="{}" target="_blank"><img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 50%;" /><br>Ver imagem</a>', obj.foto_url, obj.foto_url)
+        elif obj.foto_perfil:
+            return format_html('<a href="{}" target="_blank"><img src="{}" width="50" height="50" style="object-fit: cover; border-radius: 50%;" /><br>Ver imagem</a>', obj.foto_perfil.url, obj.foto_perfil.url)
+        return "Sem imagem"
+    
+    display_foto_perfil.short_description = "Foto de Perfil"
 
     class Meta:
         verbose_name = 'Usuário'
